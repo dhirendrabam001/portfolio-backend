02124c64fe888b5f2e861622c5b07ac895c48ef4
dhirendrabam001 <dhirendrabam12345@gmail.com>
2026-01-17 21:23:57 -0800

    updated inde.js file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const connectDB = require("./config/connection");
+const serverless = require("serverless-http");
 
 const PORT = process.env.PORT || 3000;
 
@@ -39,7 +40,6 @@ app.get("/", (req, res) => {
 // API ENDPOINT
 app.use("/api/user", userDataRoutes);
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server Is Running Port Number${PORT}`);
-});
+connectDB();
+
+module.exports = serverless(app);
